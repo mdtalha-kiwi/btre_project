cdf1ef904d5e7276b7fa594c4f33e409150f811a
talhakiwi <98952841+talhakiwi@users.noreply.github.com>
2022-02-08 11:22:12 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'talhaAthar@178',
+        'PASSWORD': '*********',
         'HOST': 'localhost'
     }
 }
